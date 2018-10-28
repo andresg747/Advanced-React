5bc5c1f84b5cbd9493cbebb54184bf29ce9f5a32
Andrés García <andres.ggarcia@mercadolibre.com>
2018-10-28 13:26:11 -0300

    Loading indicator + Nav Menu

--- sick-fits/frontend/components/Header.jsx
+++ sick-fits/frontend/components/Header.jsx
@@ -1,6 +1,20 @@
+import Router from 'next/router'
+import styled from 'styled-components'
+import NProgress from 'nprogress'
 import Nav from './Nav'
 import Link from 'next/link'
-import styled from 'styled-components'
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+} 
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+}
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+}
 
 const Logo = styled.h1`
   font-size: 4rem;
